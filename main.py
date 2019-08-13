05e0f65d996309a0bb69627aac04be1bea2e0cc5
Nathaniel Graff <nathaniel.graff@sifive.com>
2019-08-13 11:46:25 -0700

    Render ldscript template
    Signed-off-by: Nathaniel Graff <nathaniel.graff@sifive.com>

--- main.py
+++ main.py
@@ -7,7 +7,100 @@ if __name__ == "__main__":
         loader = jinja2.PackageLoader(__name__, "templates"),
     )
 
-    hello_template = env.get_template("hello.lds")
+    hello_template = env.get_template("template.lds")
 
-    print(hello_template.render(test=42))
+    default_values = {
+        "memories" : [
+            {
+                "name" : "ram",
+                "permissions" : "wxa!ri)",
+                "base" : "0x80000000",
+                "size" : "0x4000",
+            },
+            {
+                "name" : "flash",
+                "permissions" : "rxai!w",
+                "base" : "0x20010000",
+                "size" : "0x6a120",
+            },
+        ],
+        "phdrs" : [
+            {
+                "name" : "flash",
+                "type" : "PT_LOAD",
+            },
+            {
+                "name" : "ram_init",
+                "type" : "PT_LOAD",
+            },
+            {
+                "name" : "itim_init",
+                "type" : "PT_LOAD",
+            },
+            {
+                "name" : "ram",
+                "type" : "PT_LOAD",
+            },
+            {
+                "name" : "itim",
+                "type" : "PT_LOAD",
+            },
+        ],
+        "default_stack_size" : "0x400",
+        "default_heap_size" : "0x400",
+        "text_in_itim" : False,
+        "ramrodata" : False,
+        "num_harts" : 1,
+        "boot_hart" : 1,
+        "chicken_bit" : 0,
+        "init" : {
+            "vma" : "flash",
+            "lma" : "flash",
+            "phdr" : "flash",
+        },
+        "text" : {
+            "vma" : "flash",
+            "lma" : "flash",
+            "phdr" : "flash",
+        },
+        "rodata" : {
+            "vma" : "flash",
+            "lma" : "flash",
+            "phdr" : "flash",
+        },
+        "ctors" : {
+            "vma" : "flash",
+            "lma" : "flash",
+            "phdr" : "flash",
+        },
+        "itim" : {
+            "vma" : "ram",
+            "lma" : "flash",
+            "source_phdr" : "flash",
+            "dest_phdr" : "itim_init",
+        },
+        "data" : {
+            "vma" : "ram",
+            "lma" : "flash",
+            "source_phdr" : "flash",
+            "dest_phdr" : "ram_init",
+        },
+        "bss" : {
+            "vma" : "ram",
+            "lma" : "flash",
+            "phdr" : "ram_init",
+        },
+        "stack" : {
+            "vma" : "ram",
+            "lma" : "flash",
+            "phdr" : "ram_init",
+        },
+        "heap" : {
+            "vma" : "ram",
+            "lma" : "flash",
+            "phdr" : "ram_init",
+        },
+    }
+
+    print(hello_template.render(default_values))
 
